9195cca63b8400c65b9160374213d38b0c03e83a
Zoe Rorvig <zrorvig@gmail.com>
2022-12-01 22:08:52 -0500

    set up basic README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # code-quiz
 Coding Quiz Challenge
+[Link to My Project](https://zoerorvig.github.io/code-quiz/)
+
+## Table of Contents
+- [Description](#description)
+- [Visuals](#visuals)
+
+## Description 
+This webpage presents a timed coding quiz.
+
+## Visuals 
+
+The following is a screenshot of the full webpage:
+
+![screenshot](#)
+
+
+
--- script.js
+++ script.js
@@ -9,9 +9,11 @@ go back or clear high scores
 // Variables
 var timeEl = document.querySelector(".timer");
 var startQuizBtn = document.querySelector("#startQuiz");
-var secondsRemain = 75;
 
+
+// Function for timer countdown.
 function countdownTimer() {
+    var secondsRemain = 75;
     var timerInterval = setInterval(function() {
       secondsRemain--; 
       timeEl.textContent = "Timer: " + secondsRemain;
@@ -26,5 +28,5 @@ function beginQuiz(){
     countdownTimer();
 }
 
-// Add event listener to generate button
+// Add event listener to startQuiz button
 startQuizBtn.addEventListener("click", beginQuiz);
